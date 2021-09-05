1cc11830d786a7fb21dd12796ff58103169acffe
ziyad <ziyadjamalali@gmail.com>
2021-09-06 03:12:24 +0700

    done

---
+++ Belajar/dian.java
@@ -0,0 +1,10 @@
+package Belajar;
+
+public class dian {
+    public static void main(String[] args) {
+        String nama ="ziyad";
+
+        System.out.println("nama saya adalah "+nama);
+    }
+    
+}
